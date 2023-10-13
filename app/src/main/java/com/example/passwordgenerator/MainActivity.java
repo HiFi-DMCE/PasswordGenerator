7b1b23ef1a8c066566e7ce65d61402a53a7f2d53
KaustubhAnerao <kaanerao@gmail.com>
2023-10-13 17:11:43 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/passwordgenerator/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.passwordgenerator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn;
+        btn = findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(MainActivity.this, "Password Generated Successfully", Toast.LENGTH_SHORT).show();
+
+            Intent iBtn = new Intent (MainActivity.this, PasswordActivity.class);
+            startActivity(iBtn);
+        }
+    });
+}
+}
