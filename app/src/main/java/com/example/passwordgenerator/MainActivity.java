89934f310afbd532bd5ffaaf1d09eaa47571b430
n0ya6101 <142074263+n0ya6101@users.noreply.github.com>
2023-10-13 17:46:51 +0530

    Update and rename PasswordActivity.java to PasswordGenerator.java

--- app/src/main/java/com/example/passwordgenerator/MainActivity.java
+++ app/src/main/java/com/example/passwordgenerator/MainActivity.java
@@ -1,62 +1,70 @@
+
+
+
+// new merged
+
 package com.example.passwordgenerator;
 
+      
+
+        import androidx.appcompat.app.AppCompatActivity;
+
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.Toast;
+
+        import android.annotation.SuppressLint;
+
+        import android.widget.EditText;
+        import android.widget.Switch;
 
 
-import android.annotation.SuppressLint;
-import android.os.Bundle;
 
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Switch;
-import android.widget.TextView;
-
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
-
-public class MainActivity extends AppCompatActivity  {
-
-    private PasswordGenerator passwordGenerator;
+public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        passwordGenerator = new PasswordGenerator();
+        PasswordActivity passwordGenerator = new PasswordActivity();
 
         EditText passwordLengthInput = findViewById(R.id.Password_Length);
-        Button generateButton = findViewById(R.id.generate);
-        TextView passwordOutput= findViewById(R.id.newPassword);
 
-        generateButton.setOnClickListener(v -> {
-    @SuppressLint("UseSwitchCompatOrMaterialCode") Switch Lowercase = findViewById(R.id.LowerCase);
-    @SuppressLint("UseSwitchCompatOrMaterialCode") Switch Uppercase = findViewById(R.id.Uppercase);
-    @SuppressLint("UseSwitchCompatOrMaterialCode") Switch Number= findViewById(R.id.Numbers);
-    @SuppressLint("UseSwitchCompatOrMaterialCode") Switch Special = findViewById(R.id.special);
+        Button btn;
+        btn = findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                @SuppressLint("UseSwitchCompatOrMaterialCode") Switch Lowercase = findViewById(R.id.LowerCase);
+                @SuppressLint("UseSwitchCompatOrMaterialCode") Switch Uppercase = findViewById(R.id.Uppercase);
+                @SuppressLint("UseSwitchCompatOrMaterialCode") Switch Number= findViewById(R.id.Numbers);
+                @SuppressLint("UseSwitchCompatOrMaterialCode") Switch Special = findViewById(R.id.special);
+                if( (passwordLengthInput.getText().length()) <= 0) {
+                    return;
+                }
+                boolean anyToggleSelected =Lowercase.isChecked() ||
+                        Uppercase.isSelected() ||
+                        Number.isSelected() ||
+                        Special.isSelected();
 
-    if( (passwordLengthInput.getText().length()) <= 0) {
-        return;
+
+                int passwordLength = Integer.parseInt(String.valueOf(passwordLengthInput.getText()));
+                if(anyToggleSelected){
+                     passwordGenerator.generatePassword(passwordLength,
+                            Uppercase.isSelected() ,
+                            Lowercase.isChecked(),
+                            Number.isSelected(),
+                            Special.isSelected());
+
+                Toast.makeText(MainActivity.this, "Password Generated Successfully", Toast.LENGTH_SHORT).show();
+                    Intent iBtn = new Intent (MainActivity.this, PasswordActivity.class);
+                    startActivity(iBtn);
+
+            }
+        }); // TODO check for syntax/braces errors
     }
-    boolean anyToggleSelected =Lowercase.isChecked() ||
-            Uppercase.isSelected() ||
-            Number.isSelected() ||
-            Special.isSelected();
-
-
-    int passwordLength = Integer.parseInt(String.valueOf(passwordLengthInput.getText()));
-    if(anyToggleSelected){
-        String generatedPassword = passwordGenerator.generatePassword(passwordLength,
-                Uppercase.isSelected() ,
-                Lowercase.isChecked(),
-                Number.isSelected(),
-                Special.isSelected());
-
-        passwordOutput.setText(generatedPassword);}
-
-
-        });
     }
-}
 
--- app/src/main/java/com/example/passwordgenerator/PasswordActivity.java
+++ app/src/main/java/com/example/passwordgenerator/PasswordActivity.java
@@ -3,12 +3,53 @@ package com.example.passwordgenerator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class PasswordActivity extends AppCompatActivity {
+import java.util.Random;
+
+public class PasswordActivity extends AppCompatActivity  {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_password);
+    }
+
+    public static final String LOWERCASE_CHARACTERS = "abcdefghijklmnopqrstuvwxyz";
+    public static final String UPPERCASE_CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public static final String NUMBERS = "012345679";
+    public static final String SPECIAL_SYMBOLS = "!@#$%^&*()-_=+[]{};:,.<>/?";
+    private final Random random;
+    public PasswordActivity() {
+        random = new Random();
+    }
+
+    public boolean generatePassword(int length, boolean includeUppercase, boolean includeLowercase, boolean includeNumbers,
+                                    boolean includeSpecialSymbols) {
+        StringBuilder passwordBuilder = new StringBuilder();
+
+        String validCharacters = "";
+        if (includeUppercase) validCharacters += UPPERCASE_CHARACTERS;
+        if (includeLowercase) validCharacters += LOWERCASE_CHARACTERS;
+        if (includeNumbers) validCharacters += NUMBERS;
+        if (includeSpecialSymbols) validCharacters += SPECIAL_SYMBOLS;
+
+        for (int i = 0; i < length; i++) {
+            int randomIndex = random.nextInt(validCharacters.length());
+
+            char randomChar = validCharacters.charAt(randomIndex);
+
+            passwordBuilder.append(randomChar);
+
+        }
+        TextView passwordOutput;
+
+        passwordOutput = findViewById(Generated_Password); //TODO link generated password in its textview field
+
+        passwordOutput.setText(passwordBuilder.toString());
+
+
+        return true;
     }
 }
