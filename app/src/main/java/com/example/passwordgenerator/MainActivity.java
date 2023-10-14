234bb3a26f233740073ea8cf53e4fa2b91ee3b13
KaustubhAnerao <kaanerao@gmail.com>
2023-10-14 17:26:23 +0530

    3rd

--- app/src/main/java/com/example/passwordgenerator/MainActivity.java
+++ app/src/main/java/com/example/passwordgenerator/MainActivity.java
@@ -2,30 +2,97 @@ package com.example.passwordgenerator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
 
+
+public class MainActivity extends AppCompatActivity {
+    int pos =0;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Button btn;
+        SeekBar sk;
+        TextView textView;
+        Switch sw1,sw2,sw3,sw4;
+
+        textView = findViewById(R.id.textView);
         btn = findViewById(R.id.btn);
-        btn.setOnClickListener(new View.OnClickListener() {
+        sk = findViewById(R.id.sk);
+        sw1 = findViewById(R.id.sw1);
+        sw2 = findViewById(R.id.sw2);
+        sw3 = findViewById(R.id.sw3);
+        sw4 = findViewById(R.id.sw4);
+
+
+
+        sk.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+
             @Override
-            public void onClick(View v) {
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                pos=i;
+                textView.setText("Length:"+pos);
+            }
 
-                Toast.makeText(MainActivity.this, "Password Generated Successfully", Toast.LENGTH_SHORT).show();
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
 
-            Intent iBtn = new Intent (MainActivity.this, PasswordActivity.class);
-            startActivity(iBtn);
-        }
-    });
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        btn.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick (View v) {
+                if(pos>0){
+
+
+                    Boolean c1 = sw1.isChecked();
+                    Boolean c2 = sw2.isChecked();
+                    Boolean c3 = sw3.isChecked();
+                    Boolean c4 = sw4.isChecked();
+
+                    Toast.makeText(MainActivity.this, "Password Generated Successfully", Toast.LENGTH_SHORT).show();
+
+                    Intent iBtn = new Intent(MainActivity.this, PasswordActivity.class);
+                    Bundle extra = new Bundle();
+                    extra.putInt("val",pos);
+                    extra.putBoolean("1",c1);
+                    extra.putBoolean("2",c2);
+                    extra.putBoolean("3",c3);
+                    extra.putBoolean("4",c4);
+                    iBtn.putExtra("b",extra);
+                    startActivity(iBtn);}
+                else{
+                    Toast.makeText(MainActivity.this, "Invalid Length", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+        });
+
+    }
+
 }
-}
+
+
+
+
--- app/src/main/java/com/example/passwordgenerator/PasswordActivity.java
+++ app/src/main/java/com/example/passwordgenerator/PasswordActivity.java
@@ -3,12 +3,71 @@ package com.example.passwordgenerator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.security.SecureRandom;
 
 public class PasswordActivity extends AppCompatActivity {
-
+    int len ;
+    Boolean c1,c2,c3,c4;
+    TextView tV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_password);
+
+
+        Bundle extras = getIntent().getExtras();
+        len = extras.getInt("val");
+         /*c1 = extras.getBoolean("1");
+         c2 = extras.getBoolean("2");
+         c3 = extras.getBoolean("3");
+         c4 = extras.getBoolean("4");*/
+        Boolean c1 =true,c2=true,c3=true,c4=true;
+        String pass = Generate(len,c1,c2,c3,c4);
+        tV=findViewById(R.id.tv);
+
+        tV.setText(pass);
+
+        Log.d("PasswordActivity", "len: " + len);
+        Log.d("PasswordActivity", "c1: " + c1);
+        Log.d("PasswordActivity", "c2: " + c2);
+        Log.d("PasswordActivity", "c3: " + c3);
+        Log.d("PasswordActivity", "c4: " + c4);
+
+
+    }
+
+    private String Generate(int len,Boolean c1,Boolean c2,Boolean c3,Boolean c4)
+    {
+
+        String LOWERCASE_CHARACTERS = "abcdefghijklmnopqrstuvwxyz";
+        String UPPERCASE_CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String NUMBERS = "012345679";
+        String SPECIAL_SYMBOLS = "!@#$%^&*()-_=+[]{};:,.<>/?";
+        String VALID_CHARACTERS="";
+        if(c1==true) {
+            VALID_CHARACTERS += LOWERCASE_CHARACTERS;
+        }
+        if(c2==true){
+            VALID_CHARACTERS += UPPERCASE_CHARACTERS;
+        }
+        if(c3==true){
+            VALID_CHARACTERS += NUMBERS;
+        }
+        if(c4==true){
+            VALID_CHARACTERS += SPECIAL_SYMBOLS;
+        }
+
+
+        StringBuilder sb = new StringBuilder();
+        SecureRandom random = new SecureRandom();
+
+        for(int i=0;i<len;i++){
+            int index=random.nextInt(VALID_CHARACTERS.length());
+            sb.append(VALID_CHARACTERS.charAt(index));
+        }
+        return sb.toString();
     }
 }
